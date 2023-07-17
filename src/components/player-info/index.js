cd3e531c6497c33841afab2b9454722a984e7d32
Paulo Farah <paulofarahalves@hotmail.com>
2023-07-17 18:17:14 -0300

    first commit

--- src/components/player-info/index.js
+++ src/components/player-info/index.js
@@ -1,44 +1,76 @@
-import React from "react";
-import players1 from "../../players.json";
-export default function PlayerDetail({ close, i, addPlayer, selectedPlayers }) {
+import React from 'react';
+import players1 from '../../players.json';
+export default function PlayerDetail({
+	close,
+	addPlayer,
+	selectedPlayers,
+	i,
+	isOpened,
+}) {
 	const [players] = React.useState([...players1]);
+	let playerName = players[i]?.name.replace(' ', '-');
+
 	return (
 		//Style fixed to center
+
 		<div
 			className="card outlined mt-0"
 			style={{
-				position: "fixed",
-				left: "50%",
-				transform: "translateX(-50%)",
-				padding: "20px",
-				width: "500px",
-				top: "30%",
+				position: 'fixed',
+				left: '50%',
+				transform: 'translateX(-50%)',
+				padding: '20px',
+				width: '500px',
+				top: '30%',
+				display: isOpened === true ? '' : 'none',
 			}}
-			data-testid={`player-Rohit-Sharma-details`}
+			data-testid={`player-${playerName}-details`}
 		>
-			<h1 className="card-title" style={{ textAlign: "center" }}>
+			<h1 className="card-title" style={{ textAlign: 'center' }}>
 				Player Detail
 			</h1>
 			<p>
-				<strong>Name:</strong> <span data-testid="player-detail-Rohit-Sharma-name">Rohit Sharma</span>
+				<strong>Name:</strong>{' '}
+				<span data-testid={`player-detail-${playerName}-name`}>
+					{players[i]?.name}
+				</span>
 			</p>
 			<p>
-				<strong>Type:</strong> <span data-testid="player-detail-Rohit-Sharma-type">Batsman</span>
+				<strong>Type:</strong>{' '}
+				<span data-testid={`player-detail-${playerName}-type`}>
+					{players[i]?.type}
+				</span>
 			</p>
 			<p>
-				<strong>Batting:</strong> <span data-testid="player-detail-Rohit-Sharma-batting">90</span>
+				<strong>Batting:</strong>{' '}
+				<span data-testid={`player-detail-${playerName}-batting`}>
+					{players[i]?.battingSkill}
+				</span>
 			</p>
 			<p>
-				<strong>Bowling:</strong> <span data-testid="player-detail-Rohit-Sharma-bowling">90</span>
+				<strong>Bowling:</strong>{' '}
+				<span data-testid={`player-detail-${playerName}-bowling`}>
+					{players[i]?.bowlingSkill}
+				</span>
 			</p>
 			<button
-				disabled={false}
-				onClick={() => addPlayer(0)}
-				data-testid="player-detail-Rohit-Sharma-add"
+				disabled={
+					selectedPlayers.findIndex((item) => {
+						return item.name === players[i]?.name;
+					}) === -1
+						? false
+						: true
+				}
+				onClick={() => addPlayer(i)}
+				data-testid={`player-detail-${playerName}-add`}
 			>
 				Select
 			</button>
-			<button onClick={close} className="danger" data-testid="player-detail-Rohit-Sharma-close">
+			<button
+				onClick={close}
+				className="danger"
+				data-testid={`player-detail-${playerName}-close`}
+			>
 				Close
 			</button>
 		</div>
--- src/components/team-selection/index.js
+++ src/components/team-selection/index.js
@@ -1,61 +1,76 @@
-import React from "react";
+import React from 'react';
+import PlayerDetail from '../player-info';
+import playersList from '../../players.json';
 
-import PlayerDetail from "../player-info";
-import playersList from "../../players.json";
 export default function TeamSelection() {
 	const [players] = React.useState([...playersList]);
 	const [selectedPlayers, setSelectedPlayers] = React.useState([]);
 	const [showPlayerDetail, setShowPlayerDetail] = React.useState(false);
 	const [idx, setIdx] = React.useState(null);
 	const [welcome, setWelcome] = React.useState(true);
+	/*
 	const [noBat, setNoBat] = React.useState(0);
 	const [noBowl, setNoBowl] = React.useState(0);
 	const [noAR, setNoAR] = React.useState(0);
 	const [noWk, setNoWK] = React.useState(0);
+	*/
 
 	const addPlayer = (index) => {
+		if (selectedPlayers.length > 4) {
+			return;
+		} else {
+			setSelectedPlayers([...selectedPlayers, players[index]]);
+			return;
+		}
+	};
+
+	const removePlayer = (name) => {
+		setSelectedPlayers(
+			selectedPlayers.filter((player) => player.name !== name)
+		);
 		return;
 	};
 
-	const removePlayer = (index) => {
-		return
-	};
-
 	const showplayerDetailsCard = (i) => {
+		setIdx(i);
+		setShowPlayerDetail(true);
 		return;
 	};
 
 	const closeCard = () => {
+		setShowPlayerDetail(false);
 		return;
 	};
 
 	return (
 		<div className="mt-50 layout-column justify-content-center align-items-center">
-			<div style={{ display: "flex", width: "80%" }}>
-					<PlayerDetail
-						selectedPlayers={selectedPlayers}
-						i={idx}
-						close={() => closeCard()}
-						index={1}
-						addPlayer={(i) => addPlayer(i)}
-					/>
+			<div style={{ display: 'flex', width: '80%' }}>
+				<PlayerDetail
+					selectedPlayers={selectedPlayers}
+					i={idx}
+					close={() => closeCard()}
+					index={1}
+					addPlayer={(i) => addPlayer(i)}
+					isOpened={showPlayerDetail}
+				/>
+
 				<div
 					className="card outlined mt-0"
 					style={{
-						width: "50%",
-						marginRight: "10px",
-						overflow: "scroll",
-						height: "80vh",
+						width: '50%',
+						marginRight: '10px',
+						overflow: 'scroll',
+						height: '80vh',
 					}}
 				>
 					<div className="card-text">
-						<h4 style={{ textAlign: "center" }}>Available Players</h4>
+						<h4 style={{ textAlign: 'center' }}>
+							Available Players
+						</h4>
 						<table>
 							<thead>
 								<tr>
-									<th
-										data-testid="available-players-name"
-									>
+									<th data-testid="available-players-name">
 										Name
 									</th>
 									<th>Role</th>
@@ -63,43 +78,90 @@ export default function TeamSelection() {
 								</tr>
 							</thead>
 							<tbody data-testid="available-players-table-body">
-									<tr>
-										<td data-testid="selection-rules" colSpan="3" className="card pb-20">
-											<p data-testid="close-welcome" style={{textAlign:'right'}} onClick={()=>setWelcome(false)}>X</p>
-											<h3 style={{ textAlign: "center" }}>
-												<strong>Welcome to Team Selection</strong>
-											</h3>
-											11 players are required in a team <br />
-											3-6 batsmen and bowlers are allowed in a team
-											<br />
-											Only 1 Wicket Keeper required in a team
-											<br />
-											1-4 All Rounders are allowed in a team
+								<tr>
+									<td
+										data-testid="selection-rules"
+										colSpan="3"
+										className="card pb-20"
+										style={{
+											display:
+												welcome === false ? 'none' : '',
+										}}
+									>
+										<p
+											data-testid="close-welcome"
+											style={{ textAlign: 'right' }}
+											onClick={() => setWelcome(false)}
+										>
+											X
+										</p>
+										<h3 style={{ textAlign: 'center' }}>
+											<strong>
+												Welcome to Team Selection
+											</strong>
+										</h3>
+										11 players are required in a team <br />
+										3-6 batsmen and bowlers are allowed in a
+										team
+										<br />
+										Only 1 Wicket Keeper required in a team
+										<br />
+										1-4 All Rounders are allowed in a team
+									</td>
+								</tr>
+
+								{players?.map((player, k) => (
+									<tr
+										data-testid={`available-${player.name.replace(
+											' ',
+											'-'
+										)}-row`}
+										key={k}
+									>
+										<td
+											data-testid={`available-${player.name.replace(
+												' ',
+												'-'
+											)}-name`}
+											onClick={() =>
+												showplayerDetailsCard(k)
+											}
+										>
+											{player.name}
+										</td>
+										<td
+											onClick={() =>
+												showplayerDetailsCard(k)
+											}
+										>
+											{player.type}
+										</td>
+										<td>
+											<button
+												data-testid={`available-${player.name.replace(
+													' ',
+													'-'
+												)}-select`}
+												onClick={() => addPlayer(k)}
+												disabled={
+													selectedPlayers.findIndex(
+														(item) => {
+															return (
+																item.name ===
+																player.name
+															);
+														}
+													) === -1
+														? false
+														: true
+												}
+												className="btn btn-primary text"
+											>
+												Select
+											</button>
 										</td>
 									</tr>
-										<tr
-											data-testid="available-Rohit-Sharma-row"
-										>
-											<td
-												data-testid="available-Rohit-Sharma-name"
-												onClick={() => showplayerDetailsCard(0)}
-											>
-												Rohit Sharma
-											</td>
-											<td onClick={() => showplayerDetailsCard(0)}>
-												Batsman
-											</td>
-											<td>
-												<button
-													data-testid="available-Rohit-Sharma-select"
-													onClick={() => addPlayer(0)}
-													disabled={false}
-													className="btn btn-primary text"
-												>
-													Select
-												</button>
-											</td>
-										</tr>
+								))}
 							</tbody>
 						</table>
 					</div>
@@ -107,14 +169,16 @@ export default function TeamSelection() {
 				<div
 					className="card outlined mt-0"
 					style={{
-						width: "50%",
-						marginRight: "10px",
-						overflow: "scroll",
-						height: "80vh",
+						width: '50%',
+						marginRight: '10px',
+						overflow: 'scroll',
+						height: '80vh',
 					}}
 				>
 					<div className="card-text">
-						<h4 style={{ textAlign: "center" }}>Selected Players</h4>
+						<h4 style={{ textAlign: 'center' }}>
+							Selected Players
+						</h4>
 						<table>
 							<thead>
 								<tr>
@@ -128,8 +192,8 @@ export default function TeamSelection() {
 									return (
 										<tr
 											data-testid={`selected-${player.name
-												.split(" ")
-												.join("-")}-row`}
+												.split(' ')
+												.join('-')}-row`}
 											key={index}
 										>
 											<td>{player.name}</td>
@@ -137,9 +201,13 @@ export default function TeamSelection() {
 											<td>
 												<button
 													data-testid={`selected-${player.name
-														.split(" ")
-														.join("-")}-remove`}
-													onClick={() => removePlayer(index)}
+														.split(' ')
+														.join('-')}-remove`}
+													onClick={() =>
+														removePlayer(
+															player.name
+														)
+													}
 													className="btn danger text"
 												>
 													Remove
